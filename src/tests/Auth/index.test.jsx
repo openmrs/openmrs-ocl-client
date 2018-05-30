a0444d4d6126cc134bdc4dd4ec5a4bf319fb0cda
Emmanuel Ndukwe <emasysnd@gmail.com>
2018-05-30 17:23:13 +0100

    OCLOMRS-17: Protect routes that require authentication (#12)

---
+++ src/tests/Auth/index.test.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { createMockStore } from 'redux-test-utils';
+import Router from 'react-mock-router';
+
+import Authenticate from '../../components/Auth';
+import { authenticated, notAuthenticated } from '../__mocks__/fakeStore';
+import TestComponent from '../__mocks__/FakeComponent';
+
+let store = createMockStore(authenticated);
+const props = {
+  history: { push: jest.fn() },
+};
+
+describe('higher-order component', () => {
+  let WrapperComponent;
+
+  beforeEach(() => {
+    WrapperComponent = Authenticate(TestComponent);
+  });
+
+  test('[Authenticate] should render without crashing', () => {
+    const wrapper = mount(<Provider store={store}>
+      <WrapperComponent {...props} />
+    </Provider>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('[Authenticate] should render with unauthenticated data without crashing', () => {
+    store = createMockStore(notAuthenticated);
+    const wrapper = mount(<Provider store={store}>
+      <Router>
+        <WrapperComponent {...props} />
+      </Router>
+                          </Provider>);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
