20ecf9beb5658c54da749323181ecb3bd077ef28
dkayiwa <dkayiwa@openmrs.org>
2018-05-30 20:43:37 +0300

    Revert "Revert "OCLOMRS-29: Restructure app to improve code maintainability"" (#24)
    * Revert "Revert "Revert "OCLOMRS-21: Integrate Linting on CI and Prettier for better code" (#22)" (#23)"
    
    This reverts commit f2f81b07b60e7b9d11b30848ad84a5549b2f2c6a.
    
    * Revert "Revert "OCLOMRS-21: Integrate Linting on CI and Prettier for better code" (#22)"
    
    This reverts commit 5fcea0d6b57f08ebfddd39c543c4f75a54061298.
    
    * Revert "Revert "OCLOMRS-29: Restructure app to improve code maintainability" (#21)"
    
    This reverts commit f6d6d2839baee51b5929fac090fd04b77968c9d4.

---
+++ src/redux/actions/types.js
@@ -0,0 +1,5 @@
+export const AUTHENTICATED = '[auth] loggedin user';
+export const AUTHENTICATION_FAILED = '[auth] login failed';
+export const AUTHENTICATION_IN_PROGRESS = '[auth] login progess';
+export const LOGGED_OUT = '[auth] logout';
+export const LOGOUT_FAILED = '[auth] logout failed';
