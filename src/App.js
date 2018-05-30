2f2477bf0a6a5d59c486faa69ff7a76aa5462942
dkayiwa <dkayiwa@openmrs.org>
2018-05-30 20:07:33 +0300

    Revert "Revert "OCLOMRS-17: Protect routes that require authentication (#12)" (#20)"
    This reverts commit f6be69cea37284850a94485b785aa01740099dac.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import store from './redux/reducers/store';
 import './App.css';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import Authenticate from './components/Auth';
 
 const App = () => (
   <Provider store={store}>
@@ -16,7 +17,7 @@ const App = () => (
         <Navbar />
         <Switch>
           <Route exact path="/" component={Login} />
-          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/dashboard" component={Authenticate(Dashboard)} />
         </Switch>
       </div>
     </BrowserRouter>
