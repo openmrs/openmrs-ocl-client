f235a199e1a9e6adc87ef253760a5da613aef2cf
Emmanuel Abaye <emmanuel.abaye@andela.com>
2018-08-27 00:13:40 +0100

    --amend

--- src/tests/Dictionary/EditDictionary.test.jsx
+++ src/tests/Dictionary/EditDictionary.test.jsx
@@ -30,44 +30,4 @@ describe('Test suite for Edit Dictionary', () => {
     const wrapper = shallow(<Provider store={store}><EditDictionary {...props} /></Provider>);
     expect(wrapper).toMatchSnapshot();
   });
-  /*const wrapper = shallow(<DictionaryModal {...props} />);
-  const preventDefault = { preventDefault: jest.fn() };
-  const toUpperCase = { toUpperCase: jest.fn() };
-  const then = { then: jest.fn() };
-
-  it('should take a snapshot', () => {
-    expect(wrapper).toMatchSnapshot();
-    expect(wrapper.instance().componentDidMount());
-  });
-  it('Test to find and click cancel and add dictionary buttons', () => {
-    expect(wrapper.find('#cancel').simulate('click'));
-    expect(wrapper.find('#addDictionary').simulate('click', preventDefault));
-    expect(wrapper.instance().validate(preventDefault));
-  });
-  it('Tests that the component changes state', () => {
-    wrapper.setState({ errors: {} });
-    expect(wrapper.state('errors')).toEqual({});
-  });
-
-  it('Opens and closes modal on trigger', () => {
-    expect(wrapper.find('#cancel').simulate('click'));
-  });
-
-  it('Sets the state of the component to the value of the input elements on change', () => {
-    wrapper.find('#dictionary_name').simulate('change', { target: { value: 'CIEL', name: 'name' } });
-    expect(wrapper.state().data.name).toEqual('CIEL');
-  });
-
-  it('Renders component that adds a dictionary', () => {
-    const data = {
-      dictionary: {
-        name: toUpperCase,
-        submit: then,
-        then: jest.fn(),
-      },
-    };
-    const component = shallow(<AddDictionary {...props} {...preventDefault} {...data} />);
-    expect(component).toMatchSnapshot();
-  });
-  */
 });
