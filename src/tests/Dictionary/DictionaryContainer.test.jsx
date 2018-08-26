7c3f822798b6401c774aa2af579f0853d5f3f4ed
Emmanuel Abaye <emmanuel.abaye@andela.com>
2018-08-26 23:22:05 +0100

    --amend

--- src/tests/Dictionary/DictionaryContainer.test.jsx
+++ src/tests/Dictionary/DictionaryContainer.test.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createMockStore } from 'redux-test-utils'
 import { MemoryRouter } from 'react-router-dom';
 import { mount, shallow } from 'enzyme';
 import { DictionaryOverview } from '../../components/dashboard/components/dictionary/DictionaryContainer';
 import dictionary from '../__mocks__/dictionaries';
 
+const store = createMockStore({
+  organizations: {
+    organizations: [],
+  },
+});
 jest.mock('../../components/dashboard/components/dictionary/AddDictionary');
 describe('DictionaryOverview', () => {
   it('should render without any data', () => {
@@ -13,7 +20,24 @@ describe('DictionaryOverview', () => {
     const wrapper = <MemoryRouter><DictionaryOverview {...props} /></MemoryRouter>;
     expect(wrapper).toMatchSnapshot();
   });
- 
+  it('should render with dictionary data', () => {
+    const props = {
+      dictionary: [dictionary],
+      match: {
+        params: {
+          ownerType: 'testing',
+          owner: 'tester',
+          type: 'collection',
+          name: 'chris',
+        },
+      },
+      fetchDictionary: jest.fn(),
+    };
+    const wrapper = mount(<Provider store={store}><MemoryRouter><DictionaryOverview {...props} /></MemoryRouter></Provider>);
+    expect(wrapper.find('#headingDict')).toHaveLength(1);
+    expect(wrapper.find('.paragraph')).toHaveLength(5);
+    expect(wrapper).toMatchSnapshot();
+  });
   it('should render preloader spinner', () => {
     const props = {
       fetchDictionary: jest.fn(),
