c916d259721371a1eb62529dfdbe8274012726f8
Kariuki Kelvin <kelvin.kariuki@andela.com>
2018-08-30 15:18:28 +0300

    OCLOMRS-159: A user should be able to Login with username and password (#122)

--- src/redux/actions/auth/authActions.js
+++ src/redux/actions/auth/authActions.js
@@ -3,13 +3,23 @@ import { login, loginFailed, loginStarted, logout } from './authActionCreators';
 
 const BASE_URL = 'https://api.qa.openconceptlab.org/';
 
-const loginAction = ({ username, token }) => async (dispatch) => {
+const loginAction = ({ username, password }) => async (dispatch) => {
   try {
-    const url = `${BASE_URL}/users/${username}/`;
+    const data = {
+      username,
+      password,
+    };
+
+    const loginUrl = `${BASE_URL}/users/login/`;
+    const loginResponse = await axios.post(loginUrl, data);
+    const { token } = loginResponse.data;
     const headers = { Authorization: `Token ${token}` };
     dispatch(loginStarted());
+
+    const url = `${BASE_URL}/users/${username}/`;
     const response = await axios.post(url, null, { headers });
     dispatch(login(response));
+
     localStorage.setItem('token', `Token ${token}`);
     localStorage.setItem('username', `${username}`);
   } catch (error) {
