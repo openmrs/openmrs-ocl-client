48d749bf02ad62c09f68aca54ff8d86244b3de22
Kariuki Kelvin <kelvin.kariuki@andela.com>
2018-10-02 21:15:58 +0300

    OCLOMRS-220: Rework the Login UI according to the MVP (#147)

--- src/components/login/container/index.jsx
+++ src/components/login/container/index.jsx
@@ -42,54 +42,72 @@ export class Login extends Component {
   render() {
     const { username, password } = this.state;
     const { loading } = this.props;
-    const buttonTitle = loading ? 'Signing in...' : 'Sign in';
+    const buttonTitle = loading ? 'Logging in...' : 'Log in';
     return (
       <div className="container">
         <div className="row form-container">
           <div className="col-lg-5 col-md-9 col-sm-12">
+            <p className="paragraphs">
+              <h1 className="login-title">Open Concept Lab</h1>
+              <h4 className="low-title">for OpenMRS</h4>
+            </p>
+            <h6 className="low-title-msg">
+              Use the shared Open Concept Lab to create OpenMRS dictionaries by mixing
+              expert-defined content with your own custom concepts.
+            </h6>
             <form className="form-wrapper" onSubmit={this.handleLogin}>
-              <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="OCL Username"
-                  name="username"
-                  id="username"
-                  required
-                  value={username.trim()}
-                  onChange={this.handleInput}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  id="password"
-                  className="form-control"
-                  placeholder="OCL Password"
-                  name="password"
-                  required
-                  value={password.trim()}
-                  onChange={this.handleInput}
-                />
+              <fieldset>
+                <legend>Log In to Open Concept Lab</legend>
+                <div className="form-group">
+                  <label htmlFor="username">Username</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="OCL Username"
+                    name="username"
+                    id="username"
+                    required
+                    value={username.trim()}
+                    onChange={this.handleInput}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    className="form-control"
+                    placeholder="OCL Password"
+                    name="password"
+                    required
+                    value={password.trim()}
+                    onChange={this.handleInput}
+                  />
 
-              </div>
-              <SubmitButton buttonTitle={buttonTitle} disable={loading} />
-              <div className="form-group text-center pt-3">
-                <small id="tokenHelp" className="form-text text-muted">
-                  {"Don't"} have an account?{' '}
-                  <a
-                    href="https://qa.openconceptlab.org/accounts/signup/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
+                </div>
+                <SubmitButton buttonTitle={buttonTitle} disable={loading} />
+                <div className="form-group text-center pt-3">
+                  <small id="tokenHelp" className="form-text text-muted">
+                    {"Don't"} have an account?{' '}
+                    <a
+                      href="https://qa.openconceptlab.org/accounts/signup/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                     Sign up
-                  </a><br />
-                  <Link to="/loginDetails" >Having Trouble with Login?</Link>
-                </small>
+                    </a><br />
+                    <a
+                      href="https://qa.openconceptlab.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                    Go to Traditional OCL
+                    </a><br />
+                    <Link to="/loginDetails" >Having Trouble with Login?</Link>
+                  </small>
 
-              </div>
+                </div>
+              </fieldset>
             </form>
           </div>
         </div>
