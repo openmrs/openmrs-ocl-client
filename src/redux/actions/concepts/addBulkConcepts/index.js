478012d79788305632067754a8cd6a33fb32f300
Duncanian <ianduncan08@gmail.com>
2018-11-05 17:01:18 +0300

    OCLOMRS-297: Fix wrong API URL endpoints (#205)

--- src/redux/actions/concepts/addBulkConcepts/index.js
+++ src/redux/actions/concepts/addBulkConcepts/index.js
@@ -35,22 +35,13 @@ export const fetchFilteredConcepts = (source = 'CIEL', query = '') => async (
   let url = `orgs/${source}/sources/${source}/concepts/?${query}&limit=0&verbose=true`;
 
   if (datatypeList.length > 0 && classList.length > 0) {
-    url = `
-      orgs/${source}/sources/${source}/concepts/?${query}
-      &limit=0&verbose=true&datatype=${datatypeList.join(',')}&conceptClass=${classList.join(',')}
-    `;
+    url = `orgs/${source}/sources/${source}/concepts/?${query}&limit=0&verbose=true&datatype=${datatypeList.join(',')}&conceptClass=${classList.join(',')}`;
   }
   if (datatypeList.length > 0 && classList.length === 0) {
-    url = `
-      orgs/${source}/sources/${source}/concepts/?${query}
-      &limit=0&verbose=true&datatype=${datatypeList.join(',')}
-    `;
+    url = `orgs/${source}/sources/${source}/concepts/?${query}&limit=0&verbose=true&datatype=${datatypeList.join(',')}`;
   }
   if (datatypeList.length === 0 && classList.length > 0) {
-    url = `
-      orgs/${source}/sources/${source}/concepts/?${query}
-      &limit=0&verbose=true&conceptClass=${classList.join(',')}
-    `;
+    url = `orgs/${source}/sources/${source}/concepts/?${query}&limit=0&verbose=true&conceptClass=${classList.join(',')}`;
   }
 
   try {
