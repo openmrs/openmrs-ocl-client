0d56c4c1c887e1c9d0c09b314728e7b4e3f35b66
Godswill Onuoha <zezzy.godswill@gmail.com>
2018-12-17 18:47:21 +0100

    OCLOMRS-356: Implement search on bulk add concepts

--- src/tests/bulkConcepts/addBulkConcepts.test.jsx
+++ src/tests/bulkConcepts/addBulkConcepts.test.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createMockStore } from 'redux-test-utils';
 import Authenticated from '../__mocks__/fakeStore';
-import { mapStateToProps, AddBulkConcepts } from '../../components/bulkConcepts/addBulkConcepts';
+import { AddBulkConcepts } from '../../components/bulkConcepts/addBulkConcepts';
 import { mockConcepts } from '../__mocks__/concepts';
 
 const store = createMockStore(Authenticated);
@@ -18,81 +18,109 @@ const match = {
   },
 };
 describe('Add Bulk Concepts', () => {
+  const props = {
+    fetchCielConcepts: jest.fn(),
+    addExistingBulkConcepts: jest.fn(),
+    cielConcepts: mockConcepts,
+    isFetching: false,
+    match,
+    language: 'en',
+  };
+
   beforeEach(() => {
     localStorage.setItem('dictionaryName', 'OpenMRS');
   });
+
   it('Should render without crashing', () => {
-    const props = {
-      fetchCielConcepts: jest.fn(),
-      cielConcepts: [],
-      isFetching: false,
-      match,
-      addExistingBulkConcepts: jest.fn(),
-      language: 'en',
-    };
     const wrapper = mount(<MemoryRouter>
       <Provider store={store}>
         <AddBulkConcepts {...props} />
       </Provider>
     </MemoryRouter>);
-    expect(wrapper).toMatchSnapshot();
-  });
-  it('calls the handleClick function when the CIEL radio button is clicked', () => {
-    const props = {
-      fetchCielConcepts: jest.fn(),
-      cielConcepts: [],
-      isFetching: false,
-      match,
-      addExistingBulkConcepts: jest.fn(),
-      language: 'en',
-    };
-    const wrapper = mount(<MemoryRouter>
-      <Provider store={store}>
-        <AddBulkConcepts {...props} />
-      </Provider>
-    </MemoryRouter>);
-    wrapper.find('#ciel').at(0).simulate('click');
-  });
-  it('calls the handleClick function when the Add all button is clicked', () => {
-    const props = {
-      fetchCielConcepts: jest.fn(),
-      addExistingBulkConcepts: jest.fn(),
-      cielConcepts: [],
-      isFetching: false,
-      match,
-      language: 'en',
-    };
-    const wrapper = mount(<MemoryRouter>
-      <Provider store={store}>
-        <AddBulkConcepts {...props} />
-      </Provider>
-    </MemoryRouter>);
-    wrapper.find('#btn-add-all').at(0).simulate('click');
-  });
-  it('enables the Add all button when a concept is selected', () => {
-    const props = {
-      fetchCielConcepts: jest.fn(),
-      addExistingBulkConcepts: jest.fn(),
-      cielConcepts: [],
-      isFetching: false,
-      match,
-      language: 'en',
-    };
-    const wrapper = shallow(<AddBulkConcepts {...props} />);
-    wrapper.setState({
-      cielConcepts: [{
-        'concept-url': 'random',
-      }],
-    });
-    expect(wrapper.find('.btn-add-all').length).toBe(1);
+    expect(wrapper.length).toEqual(1);
   });
 
-  it('should test mapStateToProps', () => {
-    const initialState = {
-      cielConcepts: {
-        cielConcepts: ['1'],
-      },
-    };
-    expect(mapStateToProps(initialState).cielConcepts).toEqual(['1']);
+  it('calls the handleClick function when the CIEL radio button is clicked', () => {
+    const wrapper = mount(<MemoryRouter>
+      <Provider store={store}>
+        <AddBulkConcepts {...props} />
+      </Provider>
+    </MemoryRouter>);
+    const bulkWrapper = wrapper.find('AddBulkConcepts').instance();
+    const spy = jest.spyOn(bulkWrapper, 'handleCielClick');
+    bulkWrapper.forceUpdate();
+    wrapper.find('#ciel').at(0).simulate('click');
+    expect(spy).toHaveBeenCalled();
   });
+
+  it('shows suggestions when typing', () => {
+    const hasMenu = wrapper => wrapper.find("#search-ul").length === 1;
+
+    const wrapper = mount(<MemoryRouter>
+      <Provider store={store}>
+        <AddBulkConcepts {...props} />
+      </Provider>
+    </MemoryRouter>);
+    wrapper.find('#search').simulate('keydown', { key: 'ArrowDown' });
+    expect(hasMenu(wrapper)).toBe(true);
+  });
+
+  it('can search for and select "Bronze ..."', () => {
+    const wrapper = mount(<MemoryRouter>
+      <Provider store={store}>
+        <AddBulkConcepts {...props} />
+      </Provider>
+    </MemoryRouter>);
+    const input = wrapper.find('#search');
+    input.simulate('change', { target: { value: '' } });
+    input.simulate('keydown', { key: 'ArrowDown' });
+    input.simulate('keydown', { key: 'Enter' });
+    expect(input.instance().value).toEqual('Bronze Diabetes');
+    const txtInput = wrapper.find('#idsText').at(0);
+    expect(txtInput.instance().props.value).toEqual('1');
+  });
+
+  it('can search for concept that does not exist', () => {
+    const wrapper = mount(<MemoryRouter>
+      <Provider store={store}>
+        <AddBulkConcepts {...props} />
+      </Provider>
+    </MemoryRouter>);
+    const input = wrapper.find('#search');
+    input.simulate('change', { target: { value: 'non existent' } });
+    input.simulate('keydown', { key: 'ArrowDown' });
+    input.simulate('keydown', { key: 'Enter' });
+    const txtInput = wrapper.find('#idsText').at(0);
+    expect(txtInput.instance().props.value).toEqual('');
+  });
+
+  it('adding ides to already existing conceptIds should prepend commer', () => {
+    const wrapper = mount(<MemoryRouter>
+      <Provider store={store}>
+        <AddBulkConcepts {...props} />
+      </Provider>
+    </MemoryRouter>);
+    const component = wrapper.find('AddBulkConcepts').instance();
+    component.handleSelected({ id: 12 });
+    expect(component.state.conceptIds).toEqual(12);
+    component.handleSelected({ id: 99 });
+    expect(component.state.conceptIds).toEqual('12, 99');
+  });
+
+  it('simulate textarea text change', () => {
+    const wrapper = mount(<MemoryRouter>
+      <Provider store={store}>
+        <AddBulkConcepts {...props} />
+      </Provider>
+    </MemoryRouter>);
+
+    const txtInput = wrapper.find('#idsText').at(0);
+    const bulkWrapper = wrapper.find('AddBulkConcepts').instance();
+    bulkWrapper.forceUpdate();
+
+    txtInput.simulate('change', { target: {value: '8,8,89' } });
+    txtInput.simulate('keydown', { key: 'Enter' });
+    expect(bulkWrapper.state.conceptIds).toEqual('8,8,89');
+  });
+
 });
