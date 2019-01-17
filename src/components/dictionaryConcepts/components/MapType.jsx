36b5580c37776159f852a13723dc9221934923a9
AGMETEOR <guwats10@gmail.com>
2019-01-17 14:57:10 +0300

    OCLOMRS-387: Make the CIEL select dropdown a text input field

---
+++ src/components/dictionaryConcepts/components/MapType.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { INTERNAL_MAPPING_DEFAULT_SOURCE } from './helperFunction';
+
+const mapType = (props) => {
+  const {
+    source, index, map_type, updateEventListener,
+  } = props;
+  const mapTypeInput = source === INTERNAL_MAPPING_DEFAULT_SOURCE
+    ? (<textarea
+      type="text"
+      rows="3"
+      className="form-control concept-description"
+      name="map_type"
+      onChange={updateEventListener}
+    />)
+    : (
+      <select
+        tabIndex={index}
+        defaultValue={map_type}
+        className="form-control"
+        placeholder="map type"
+        type="text"
+        name="map_type"
+        onChange={updateEventListener}
+      >
+        <option>Same as</option>
+        <option>Narrower than</option>
+      </select>
+    );
+  return mapTypeInput;
+};
+
+mapType.propTypes = {
+  source: PropTypes.string,
+  index: PropTypes.number,
+  updateEventListener: PropTypes.func,
+};
+
+mapType.defaultProps = {
+  source: '',
+  index: 0,
+  updateEventListener: () => {},
+};
+
+export default mapType;
