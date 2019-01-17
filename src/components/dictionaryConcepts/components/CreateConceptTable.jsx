b9f82d3d91ee94cc21eba99bfd6e793ef8b3a2c0
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-01-17 10:13:58 +0300

    OCLOMRS-388: Fix the table format on the create new concept page

--- src/components/dictionaryConcepts/components/CreateConceptTable.jsx
+++ src/components/dictionaryConcepts/components/CreateConceptTable.jsx
@@ -3,28 +3,26 @@ import PropTypes from 'prop-types';
 import ConceptNameRows from './ConceptNameRows';
 
 const CreateConceptTable = props => (
-  <div className="concept-table ">
-    <table className="table table-striped table-bordered concept-form-table">
-      <thead className="header text-white">
-        <tr>
-          <th scope="col">Name</th>
-          <th scope="col">Type</th>
-          <th scope="col">Language</th>
-          <th scope="col">Preferred in language</th>
-          <th scope="col">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.existingConcept && props.existingConcept.names
-          ? props.existingConcept.names.map(newRow => (
-            <ConceptNameRows newRow={newRow} key={newRow.uuid} {...props} />
-          ))
-          : props.nameRows.map(newRow => (
-            <ConceptNameRows key={newRow} {...props} />
-          ))}
-      </tbody>
-    </table>
-  </div>
+  <table className="table table-striped table-bordered concept-form-table">
+    <thead className="header text-white">
+      <tr>
+        <th scope="col">Name</th>
+        <th scope="col">Type</th>
+        <th scope="col">Language</th>
+        <th scope="col">Preferred in language</th>
+        <th scope="col">Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.existingConcept && props.existingConcept.names
+        ? props.existingConcept.names.map(newRow => (
+          <ConceptNameRows newRow={newRow} key={newRow.uuid} {...props} />
+        ))
+        : props.nameRows.map(newRow => (
+          <ConceptNameRows key={newRow} {...props} />
+        ))}
+    </tbody>
+  </table>
 );
 
 CreateConceptTable.propTypes = {
