f7d8146fa1b648ad676eeff1e5952d5dea3f2dc4
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com>
2019-03-08 16:55:29 +0300

    OCLOMRS-458: Implement reading an OCL backend URL from a system environment property called OCL_API_HOST (#348)

---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+export default {
+  OCL_API_HOST: process.env.REACT_APP_OCL_HOST || 'https://api.qa.openconceptlab.org/',
+  TRADITIONAL_OCL: process.env.REACT_APP_TRADITIONAL_OCL_HOST || 'https://qa.openconceptlab.org',
+};
