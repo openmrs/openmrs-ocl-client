a73155dd51f4d8ebdc2d7ed0e6f278a1d5a82670
alexkayabula <31479458+alexkayabula@users.noreply.github.com>
2019-05-02 21:01:00 +0300

    OCLOMRS-547: Remove/Update the content of the Having trouble with Login link on the login page (#427)

--- src/components/login/container/LoginDetails.jsx
+++ src/components/login/container/LoginDetails.jsx
@@ -1,31 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import newVideo from '../styles/video/newInstructions.mp4';
 import { TRADITIONAL_OCL_HOST } from '../../dictionaryConcepts/components/helperFunction';
 
 const LoginDetails = () => (
   <div className=" container login-details">
     <div className="card">
       <div className="card-header">
-        <strong>What to ensure</strong>
+        <strong>Help</strong>
       </div>
       <div className="card-body">
-        Ensure you have an account from the traditional OCL. After that,
+        Ensure you have an account or
         {' '}
-        <a href={`${TRADITIONAL_OCL_HOST}/accounts/login/`}>
-          Login into the traditional OCL
+        <a href={`${TRADITIONAL_OCL_HOST}/accounts/signup/`}>
+          sign up here
         </a>
         <br />
-        After you have logged in, you will see a user dashboard page with your
-        API token in the left corner. Then use it to Login to the OCL for
-        OpenMRS
+        After signing up, check the email address you used during sign up for the
+        confirmation email.
+        <br />
+        To activate your account, please follow the link in the
+        confirmation email to confirm your email address.
       </div>
-      <div className="card-header">
-        <strong>Get started with this short recording</strong>
-      </div>
-      <div className="card-body" />
-      <video controls muted src={newVideo} className="video" />
-      <br />
       <Link to="/">Ready to Login now?</Link>
     </div>
   </div>
