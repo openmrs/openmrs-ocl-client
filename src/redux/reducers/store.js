625098ce103ba0b3f04f59b90f31b30a9e9dd79a
Lincoln Karuhanga <karuhanga475@gmail.com>
2019-05-07 09:18:58 +0300

    OCLOMRS-541: Update state persistence to introduce state versioning

--- src/redux/reducers/store.js
+++ src/redux/reducers/store.js
@@ -4,21 +4,31 @@ import reduxThunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from './index';
 
+export const STORE_VERSION = '1';
+export const CURRENT_STORE_VERSION_KEY = 'currentStoreVersion';
+
 const saveState = (state) => {
   try {
     localStorage.setItem('state', JSON.stringify(state));
+    localStorage.setItem(CURRENT_STORE_VERSION_KEY, STORE_VERSION);
     return undefined;
   } catch (e) {
     return undefined;
   }
 };
 
-const loadState = () => {
+export const loadState = () => {
   try {
     const state = localStorage.getItem('state');
     if (state === null) {
       return undefined;
     }
+
+    const currentStoreVersion = localStorage.getItem(CURRENT_STORE_VERSION_KEY);
+    if (currentStoreVersion !== STORE_VERSION) {
+      return undefined;
+    }
+
     return JSON.parse(state);
   } catch (e) {
     return undefined;
