ca7c56a5a818885969fe14715e51d4a328b48e6d
Dan Uluma <danuluma@gmail.com>
2019-05-17 15:57:43 +0300

    OCLOMRS-563: Create Another Kind of concept should have all possible options (#448)

--- src/tests/dictionaryConcepts/components/CreateConceptForm.test.jsx
+++ src/tests/dictionaryConcepts/components/CreateConceptForm.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import CreateConceptForm from '../../../components/dictionaryConcepts/components/CreateConceptForm';
 import { mockSource } from '../../__mocks__/concepts';
 import { CONCEPT_CLASS } from '../../../components/dictionaryConcepts/components/helperFunction';
@@ -109,5 +109,37 @@ describe('Test suite for CreateConceptForm', () => {
     const wrapper = shallow(<CreateConceptForm {...props} />);
     expect(wrapper.find('.form-group.answer')).toHaveLength(1);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render all the components to display all the possible options available when creating another kind of concept', () => {
+    const props = {
+      state: {
+        id: '1',
+      },
+      concept: '',
+      addDescription: jest.fn(),
+      handleNewName: jest.fn(),
+      path: '',
+      toggleUUID: jest.fn(),
+      handleChange: jest.fn(),
+      handleSubmit: jest.fn(),
+      editable: false,
+      nameRows: [],
+      description: [],
+      addAnswer: jest.fn(),
+      answer: [],
+      disableButton: false,
+      allSources: [mockSource],
+      mappings: [{
+        id: 'uniqueid',
+        retired: false,
+      }],
+    };
+    const wrapper = mount(<CreateConceptForm {...props} />);
+    expect(wrapper.find('CreateConceptTable').length).toBe(1);
+    expect(wrapper.find('DescriptionTable').length).toBe(1);
+    expect(wrapper.find('AnswersTable').length).toBe(2);
+    expect(wrapper.find('CreateMapping').length).toBe(1);
+    wrapper.unmount();
   });
 });
