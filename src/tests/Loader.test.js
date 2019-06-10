fd0f1397cf9d59e0f28cdc0c98b40a3770ebffa7
Dan Uluma <danuluma@gmail.com>
2019-06-10 06:12:59 +0300

    OCLOMRS-605: Raise Test Coverage

--- src/tests/Loader.test.js
+++ src/tests/Loader.test.js
@@ -2,10 +2,25 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Loader from '../components/Loader';
 
-const wrapper = mount(<Loader />);
-
 describe('Loader Component', () => {
-  it('should render without crashing', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<Loader />);
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
+  it('should render a loader with classname of \'loader\'', () => {
     expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('div').props().className).toEqual('loader');
+  });
+
+  it('should render a smaller loader when \'smaller\' is true', () => {
+    const props = { smaller: true };
+    wrapper = mount(<Loader {...props} />);
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('div').props().className).toEqual('smaller-loader');
   });
 });
