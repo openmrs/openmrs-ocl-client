0b4f263074b629f7f99cabe432c8e6467eb52e2c
Lincoln Karuhanga <karuhanga475@gmail.com>
2019-10-08 14:30:28 +0300

    OCLOMRS-723: Add a little delay after creating and editing a concept to allow the search index to update

--- src/redux/actions/concepts/dictionaryConcepts.js
+++ src/redux/actions/concepts/dictionaryConcepts.js
@@ -49,7 +49,7 @@ import {
 import instance from '../../../config/axiosConfig';
 import api from '../../api';
 import { recursivelyFetchConceptMappings } from './addBulkConcepts';
-import { buildPartialSearchQuery, checkErrorMessage } from '../../../helperFunctions';
+import { buildPartialSearchQuery, checkErrorMessage, delay } from '../../../helperFunctions';
 
 export const createNewName = () => (dispatch) => {
   const payload = uuid();
@@ -412,6 +412,7 @@ export const createNewConcept = (data, dataUrl, ownerType = 'users', owner = loc
   } finally {
     if (createdConcept) {
       await dispatch(addConceptToDictionary(createdConcept.id, dataUrl));
+      await delay(1000);
       dispatch(isSuccess(createdConcept, CREATE_NEW_CONCEPT));
     }
   }
@@ -508,6 +509,7 @@ export const updateConcept = (conceptUrl, data, history, source, concept, collec
 
     dispatch(isSuccess(response.data, UPDATE_CONCEPT));
     notify.show('Concept successfully updated', 'success', 3000);
+    await delay(1000);
     dispatch(isFetching(false));
     return response.data;
   } catch (error) {
