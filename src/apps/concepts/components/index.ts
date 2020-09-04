b2ed77fe46be448c4ef1cfa663d3d73669544dce
Himabindu T <tbindu@thoughtworks.com>
2020-09-04 16:17:15 +0530

    Tarun, Bindu | OCLOMRS-881 | Create new concepts within a Source (#36)
    * Tarun, Bindu | OCLOMRS-881 | Add create concept button to create custom concepts in source
    
    * Tarun, Bindu | OCLOMRS-881 | Refactored code and added new unit tests
    
    * Tarun, Bindu | OCLOMRS-881 | Add snapshot tests for AddConceptsIcon component and refactored ViewConceptsPage test cases
    
    * Bindu, Tarun | OCLOMRS-881 | Handled feedback changes

--- src/apps/concepts/components/index.ts
+++ src/apps/concepts/components/index.ts
@@ -1,3 +1,4 @@
 export { default as ConceptForm } from "./ConceptForm";
 export { default as ConceptsTable } from "./ConceptsTable";
 export { default as ViewConceptsHeader } from "./ViewConceptsHeader";
+export { default as AddConceptsIcon } from "./AddConceptsIcon";
