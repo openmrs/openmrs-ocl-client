1896224a3f6e494b329be50261fdb3e21ca5849b
Kirity Mahanty <kirity.mahanty@thoughtworks.com>
2020-09-22 00:23:04 +0530

    Kirity | OCLOMRS-883 | Fixed for Integration tests

--- src/apps/concepts/pages/tests/e2e/ViewConceptsPage.test.ts
+++ src/apps/concepts/pages/tests/e2e/ViewConceptsPage.test.ts
@@ -207,14 +207,20 @@ describe("View Concepts Page", () => {
       cy.findByText(TEXT.IMPORT_EXISTING_CONCEPT).click();
       cy.findByText(TEXT.PICK_CONCEPTS).click();
 
-      cy.runAndAwait(() =>
-        cy
-          .findByPlaceholderText("Search concepts")
-          .type("984 Immunizations{enter}")
+      cy.runAndAwait(
+        () => {
+          cy.findByPlaceholderText("Search concepts").type(
+            "984 Immunizations{enter}"
+          );
+        },
+        "GET",
+        true
       );
+
       cy.findAllByTitle("More actions")
         .first()
         .click();
+
       cy.runAndAwait(
         () => {
           cy.findByText(TEXT.ADD_TO_DICTIONARY).click();
