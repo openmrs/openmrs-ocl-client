c6c367cb7c33c9c289709b70e2b89162676595f6
Ian <ian_bacher@brown.edu>
2020-10-18 11:01:42 -0400

    Add script to bring down the app and api

---
+++ stop_local_instance.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Stopping App..."
+docker-compose down -v || exit 1
+
+if [ -d oclapi ]; then 
+  echo "Stopping API..."
+  pushd oclapi
+  pushd oclapi >/dev/null
+  docker-compose down -v || exit 1
+  popd >/dev/null
+fi
