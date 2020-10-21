2e8b8f5f1e950cc224b89d795d802e741e6d3f7d
Ian <52504170+ibacher@users.noreply.github.com>
2020-10-21 21:09:54 +0000

    Restore updated build with fixed healthcheck
    1. Update from building on Node 10.13 -> 12.19.0 (the current LTS release)
    2. Ability to run integration tests on non-Linux x64 platforms
    3. Fail the build when the environment fails to be initialised
    4. Adds package-lock.json to the repo

--- start_local_instance.sh
+++ start_local_instance.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
 echo "Starting API..."
-git clone https://github.com/OpenConceptLab/oclapi.git
-cd oclapi || exit
-docker-compose build
-docker-compose up -d
+if [ ! -d oclapi ]; then
+  git clone https://github.com/OpenConceptLab/oclapi.git
+elif [ -d oclapi/.git ]; then
+  git --git-dir=oclapi/.git --work-tree=oclapi pull --ff-only --rebase --autostash
+else
+  echo "Directory oclapi does not seem to be a valid git repository"
+  echo "Please remove the oclapi directory"
+  exit 1
+fi
+[ $? -ne 0 ] && exit 1
+pushd oclapi >/dev/null 2>&1 || exit 1
+docker-compose build || exit $?
+docker-compose up -d || exit $?
 api_endpoint=http://localhost:8000
-cd ..
+popd >/dev/null 2>&1 || exit 1
 
-echo "Starting App..."
+echo "Building App..."
 export OCL_API_HOST=$api_endpoint
-docker-compose build
-docker-compose up -d
+docker-compose build || exit $?
+echo "Starting App..."
+docker-compose up -d || exit $?
 app_endpoint=http://localhost:8080
 
-
 # wait for the api and app to be live
-bash ./wait_for_url.sh $api_endpoint"/sources"
+./wait_for_url.sh $api_endpoint"/sources" || exit $?
 echo "API listening at "$api_endpoint
 
-bash ./wait_for_url.sh $app_endpoint
+./wait_for_url.sh $app_endpoint || exit $?
 echo "App listening at "$app_endpoint
