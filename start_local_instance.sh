5c3409bf9c1f26a4aa0a0f77df32151e65aef3da
Ian <ian_bacher@brown.edu>
2020-10-21 23:04:49 -0400

    Revert "Updated build (#633)"
    This reverts commit b2179d4accce1db19ff7037af8dee36952e3fef5.

--- start_local_instance.sh
+++ start_local_instance.sh
@@ -1,32 +1,23 @@
 #!/bin/sh
 
 echo "Starting API..."
-if [ ! -d oclapi ]; then
-  git clone https://github.com/OpenConceptLab/oclapi.git
-elif [ -d oclapi/.git ]; then
-  git --git-dir=oclapi/.git --work-tree=oclapi pull --ff-only --rebase --autostash
-else
-  echo "Directory oclapi does not seem to be a valid git repository"
-  echo "Please remove the oclapi directory"
-  exit 1
-fi
-[ $? -ne 0 ] && exit 1
-pushd oclapi >/dev/null 2>&1 || exit 1
-docker-compose build || exit $?
-docker-compose up -d || exit $?
+git clone https://github.com/OpenConceptLab/oclapi.git
+cd oclapi || exit
+docker-compose build
+docker-compose up -d
 api_endpoint=http://localhost:8000
-popd >/dev/null 2>&1 || exit 1
+cd ..
 
-echo "Building App..."
-export OCL_API_HOST=$api_endpoint
-docker-compose build || exit $?
 echo "Starting App..."
-docker-compose up -d || exit $?
+export OCL_API_HOST=$api_endpoint
+docker-compose build
+docker-compose up -d
 app_endpoint=http://localhost:8080
 
+
 # wait for the api and app to be live
-./wait_for_url.sh $api_endpoint"/sources" || exit $?
+bash ./wait_for_url.sh $api_endpoint"/sources"
 echo "API listening at "$api_endpoint
 
-./wait_for_url.sh $app_endpoint || exit $?
+bash ./wait_for_url.sh $app_endpoint
 echo "App listening at "$app_endpoint
--- wait_for_url.sh
+++ wait_for_url.sh
@@ -1,36 +1,27 @@
 #!/bin/sh
 
-curl -V > /dev/null
-if [ $? -ne 0 ]; then
-  echo "$0 requires curl to be installed"
-  exit 1
-fi
-
-if [ -n "$1" ]; then
+if [ "$1" != "" ]; then
   url=$1
 else
-  echo "Usage: $0 url [max_attempts]"
+  echo "Usage: ./wait_for_url.sh url [max_attempts]"
   exit 1
 fi
 
-if [ -n "$2" ]; then
+if [ "$2" != "" ]; then
   max_attempts=$2
 else
-  max_attempts=5
-fi
-
-if [ -z "$max_attempts" -o $max_attempts -lt 1 ]; then
-  max_attempts=5
+  max_attempts=60
 fi
 
 echo "Waiting for $1"
 
-attempt_counter=1
+attempt_counter=0
 until $(curl --output /dev/null --silent --head --fail $url); do
   if [ ${attempt_counter} -eq ${max_attempts} ];then
     echo "Max attempts reached"
     exit 1
   fi
-  printf "."
-  attempt_counter=$(expr $attempt_counter + 1)
+  printf '.'
+  attempt_counter=$(($attempt_counter+1))
+  sleep 5
 done
