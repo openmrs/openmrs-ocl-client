b132ecc97a62e7cf56c2ce73302de24cc30cdbde
Ian <ian_bacher@brown.edu>
2021-07-08 11:39:42 -0400

    Modify utilities to use environment variables rather than local variables
    Hopefully this allows tests to be run with some degree of parallelization

--- cypress/support/utils.ts
+++ cypress/support/utils.ts
@@ -1,28 +1,25 @@
 import { Store } from "redux";
 import { AppState } from "../../src/redux/types";
 
-const user = Cypress.env("USERNAME") || "admin";
-let dictionaryId: string = "";
-
 export const getStore = (): Cypress.Chainable<Store<AppState>> =>
   cy.window().its("store");
 export const isLoggedIn = (): Cypress.Chainable<boolean> =>
   getStore()
     .invoke("getState")
     .its("auth")
-    .its("isLoggedIn") || false;
+    .its("isLoggedIn") || cy.wrap(false);
 export const currentUser = (): Cypress.Chainable<string> =>
   getStore()
     .invoke("getState")
     .its("auth")
     .its("profile")
-    .its("username") || "admin";
+    .its("username") || cy.wrap("admin");
 export const getAuthToken = () =>
   getStore()
     .invoke("getState")
     .its("auth")
     .its("token")
     .then(token => `Token ${token}`);
-export const getDictionaryId = () => dictionaryId;
-export const setDictionaryId = (dictId: string) => dictionaryId = dictId;
-export const getUser = () => user;
+export const getDictionaryId = () => Cypress.env("dictionaryId");
+export const setDictionaryId = (dictionaryId: string) => Cypress.env("dictionaryId", dictionaryId);
+export const getUser = () => Cypress.env("USER") || "admin";
