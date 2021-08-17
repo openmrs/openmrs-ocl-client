555de2d16a9a658759047262420786a9e2f5e3f8
jwnasambu <julietwamalwa@yahoo.com>
2021-08-17 23:41:47 +0300

    OCLOMRS-749:The Basic User: AddBulkConcepts to the dictionary

---
+++ cypress/integration/dictionary/addBulkConceptsToDictionary.feature
@@ -0,0 +1,42 @@
+Feature: Add bulk concepts to an existing dictionary
+  Background:
+    Given the user is logged in
+
+  @dictionary
+  Scenario: The user should be able to add bulk concepts from their preferred source
+    Given the user is on the view dictionary concepts page
+     When the user clicks the "Add concepts" button
+      And the user selects "Import existing concept"
+      And the user selects "Add bulk concepts"
+     Then the user should be on the "Add concepts in bulk from CIEL" page
+
+  @dictionary
+  @ciel
+  Scenario: The user should be able to add a single bulk concept from CIEL
+    Given the user is on the "Add concepts in bulk from CIEL" page
+     When the user enters concept Id "1000"
+      And the user clicks the "ADD CONCEPTS" button
+      Then the user navigates to the "Progess notification" page
+      And the concept Id "1000" should be in the dictionary
+      
+  @dictionary
+  @ciel
+  Scenario: The user should be able to add multiple bulk concepts from CIEL
+    Given the user is on the "Add concepts in bulk from CIEL" page
+      When the user enters concept Id "1001"
+      And the user enters concept Id "1002"
+      And the user clicks the "ADD CONCEPTS" button
+      Then the user navigates to the "Progess notification" page
+       And the concept Id "1001" should be in the dictionary
+       And the concept Id "1002" should be in the dictionary
+
+@dictionary
+@ciel
+  Scenario: The system should be able to skip an already added bulk concepts from CIEL
+    Given CIEL concept "1000" is already in the dictionary
+      And the user is on the "Add concepts in bulk from CIEL" page
+      And the user enters concept Id 1000
+      And the user clicks the "ADD CONCEPTS" button
+    Then the user navigates to the "Progess notification" page
+      Then concept Id "1000" should be skippped 
+                
---
+++ cypress/support/step_definitions/dictionary/addBulkConceptsToDictionary/addBulkConceptsToDictionary.ts
@@ -0,0 +1,134 @@
+/// <reference types="cypress" />
+/// <reference types="../../../" />
+import { 
+  After, 
+  Before, 
+  Given, 
+  Then, 
+  When } from "cypress-cucumber-preprocessor/steps";
+import { getDictionaryId, getUser } from '../../../utils';
+
+Given("the user is on the view dictionary concepts page", () => {
+  cy.visit(`/users/${getUser()}/collections/${getDictionaryId()}/concepts/`);
+  cy.url().should(
+    "contain",
+    `/users/${getUser()}/collections/${getDictionaryId()}/concepts/`
+  );
+});
+
+Given('the user is on the "Add concepts in bulk from CIEL" page', () => {
+  cy.visit(
+    `/users/${getUser()}/collections/${getDictionaryId()}/add/?fromSource=CIEL/`
+  );
+  
+  cy.url().should(
+    "contain",
+    `/users/${getUser()}/collections/${getDictionaryId()}/add/?fromSource=CIEL/`
+  );
+});
+
+Given('CIEL concept "1000" is already in the dictionary', () => {
+  cy.getConcept(`/users/${getUser()}/collections/${getDictionaryId()}/`, "1000");
+});
+
+When('the user clicks the "Add concepts" button', () => {
+  cy.findByTitle("Add concepts").click();
+});
+
+When('the user selects "Add bulk concepts"', () => {
+  cy.visit(
+    `/users/${getUser()}/collections/${getDictionaryId()}/add/?fromSource=CIEL/`
+  );
+});
+
+Then('the user navigates to the "Progess notification" page', () => {
+  cy.visit(
+    `/actions/`
+  );
+  cy.url().should("contain", `/actions`);
+});
+
+When('the user enters concept Id 1000', () => {
+    cy.get("textarea").type("1000")
+  }
+);
+
+When('the user clicks the "ADD CONCEPTS" button', () => {
+  cy.findByText("Add concepts").click();
+});
+
+When(
+  /the user enters concept Id "(1000|1001|1002)"/, () => {
+    // eslint-disable-next-line no-sequences
+    cy.get("textarea").type(`1000, 1001, 1002`);
+  }
+); 
+ 
+Then('the user selects "Import existing concept"', () => {
+   cy.findByText("Import existing concept").click();
+});
+
+Then('the user should be on the "Add concepts in bulk from CIEL" page', () => {
+   cy.url().should("contain", `/users/${getUser()}/collections/${getDictionaryId()}/add/?fromSource=CIEL/`);
+});
+
+Then(
+  /the concept Id "1000" should be in the dictionary/, conceptId => {
+    cy.getConcept(`/users/${getUser()}/collections/${getDictionaryId()}/concepts/${conceptId}`).should("exist");
+  });
+
+Then(
+  /the concept Id "(1000|1001|1002)" should be in the dictionary/, conceptId => {
+    cy.getConcept(`/users/${getUser()}/collections/${getDictionaryId()}/concepts/${conceptId}/`, "1000, 1001, 1002");
+    cy.get('["2-concepts-were-added-message"]').should("be.visible");
+  });    
+
+Then('the "1000" concept should be skippped', conceptId => {
+   cy.getConcept(`/users/${getUser()}/collections/${getDictionaryId()}/concepts/${conceptId}`).should("skip");
+   cy.get('["Adding-concept-skipped-message"]').should("be.visible");
+});   
+
+Before({ tags: "@ciel" }, () => {
+   cy.createOrganisation("CIEL", true)
+    .createOrgSource("CIEL", "CIEL", true)
+    .createConcept(
+       [
+        {
+           name: "Serum",
+           locale: "en",
+           locale_preferred: true,
+           name_type: "FULLY_SPECIFIED"
+         }
+       ],
+       "/orgs/CIEL/sources/CIEL/",
+       "1001"
+     )
+     .createConcept(
+       [
+         {
+           name: "Whole blood sample",
+           locale: "en",
+           locale_preferred: true,
+           name_type: "FULLY_SPECIFIED"
+         }
+       ],
+       "/orgs/CIEL/sources/CIEL/",
+       "1000"
+     )
+     .createConcept(
+       [
+         {
+           name: "Plasma",
+           locale: "en",
+           locale_preferred: true,
+           name_type: "FULLY_SPECIFIED"
+         }
+       ],
+       "/orgs/CIEL/sources/CIEL/",
+       "1002"
+     );
+ });
+
+ After({ tags: "@ciel" }, () => {
+  cy.deleteOrgSource("CIEL", "CIEL", true).deleteOrganisation("CIEL", true);
+});
