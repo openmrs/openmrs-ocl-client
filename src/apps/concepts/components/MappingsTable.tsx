72e7409cf8499d651ab6b49ee6868791fcdd04b9
Ian <ian_bacher@brown.edu>
2022-01-10 13:13:59 -0500

    Fix OCLOMRS-1068

--- src/apps/concepts/components/MappingsTable.tsx
+++ src/apps/concepts/components/MappingsTable.tsx
@@ -69,14 +69,15 @@ const MappingsTable: React.FC<Props> = ({
 }) => {
   const classes = useStyles();
   const [sorted, setSorted] = useState<Mapping[]>();
-  const [isAnswer, setAnswer] = useState(false);
-  const [isSetMember, setSetMember] = useState(false);
+  const [allowReordering, setAllowReordering] = useState(false);
 
-  useEffect(() => setAnswer(title === "Answer"), [title]);
-  useEffect(() => setSetMember(title === "Set Member"), [title]);
+  useEffect(
+    () => setAllowReordering(title === "Answer" || title === "Set Members"),
+    [title]
+  );
 
   useEffect(() => {
-    if (sorted && isAnswer) {
+    if (sorted && allowReordering) {
       const values = sorted;
       values.sort((v1, v2) => {
         if (v1.extras?.sort_weight) {
@@ -98,35 +99,10 @@ const MappingsTable: React.FC<Props> = ({
       });
       setSorted(values);
     }
-  }, [sorted, isAnswer]);
+  }, [sorted, allowReordering]);
 
   useEffect(() => {
-    if (sorted && isSetMember) {
-      const values = sorted;
-      values.sort((v1, v2) => {
-        if (v1.extras?.sort_weight) {
-          if (v2.extras?.sort_weight) {
-            return (
-              (v1.extras.sort_weight as number) -
-              (v2.extras.sort_weight as number)
-            );
-          }
-
-          return 1;
-        }
-
-        if (v2.extras?.sort_weight) {
-          return -1;
-        }
-
-        return 0;
-      });
-      setSorted(values);
-    }
-  }, [sorted, isSetMember]);
-
-  useEffect(() => {
-    if (isAnswer && isSubmitting) {
+    if (allowReordering && isSubmitting) {
       for (let i = 0; i < values.length; i++) {
         handleChange(
           buildEvent(`${valuesKey}[${i}].extras`, { sort_weight: i + 1 })
@@ -134,24 +110,7 @@ const MappingsTable: React.FC<Props> = ({
       }
     }
   }, [
-    isAnswer,
-    isSubmitting,
-    submitCount,
-    handleChange,
-    values.length,
-    valuesKey
-  ]);
-
-  useEffect(() => {
-    if (isSetMember && isSubmitting) {
-      for (let i = 0; i < values.length; i++) {
-        handleChange(
-          buildEvent(`${valuesKey}[${i}].extras`, { sort_weight: i + 1 })
-        );
-      }
-    }
-  }, [
-    isSetMember,
+    allowReordering,
     isSubmitting,
     submitCount,
     handleChange,
@@ -233,7 +192,7 @@ const MappingsTable: React.FC<Props> = ({
               </TableCell>
             </TableRow>
           </TableHead>
-          {(isAnswer && editing) || (isSetMember && editing) ? (
+          {allowReordering && editing ? (
             <DragDropContext
               onDragEnd={param => {
                 const srcI = param.source.index;
